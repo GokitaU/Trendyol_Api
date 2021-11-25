82dd1ed5eaf9c0853137b96e61867afc762aa875
slhnklnc <aslhanklnc@gmail.com>
2021-11-25 22:33:45 +0300

    finish

--- Trendyol_Api/Trendyol_Api/Form1.cs
+++ Trendyol_Api/Trendyol_Api/Form1.cs
@@ -1,17 +1,30 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Trendyol_Api.Models;
 
 namespace Trendyol_Api
 {
     public partial class Form1 : Form
     {
+
+        //Trendyol'un tarafınıza verdiği bilgiler,kendinize gore uyarlayınız
+        //The information that trendyol gives you, adapt it according to you
+
+        static string RoleName = "12345";
+        static string RolePass = "12345";
+        static string url = "https://api.trendyol.com/sapigw/";
+        static string supplierid = "12345";
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +32,162 @@ namespace Trendyol_Api
 
         private void btnDowland_Click(object sender, EventArgs e)
         {
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+               | SecurityProtocolType.Tls11
+               | SecurityProtocolType.Tls12
+               | SecurityProtocolType.Ssl3;
+            string auth = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(RoleName + ":" + RolePass));
 
+            short page = 0;
+            short i = 0;
+            #region CreatedOrder 
+            var clientOrder = new RestClient(url + supplierid + "/orders?status=Created&page=" + page);
+            var requestOrder = new RestRequest(Method.GET);
+            requestOrder.AddHeader("Accept", "application/json;charset=utf-8");
+            requestOrder.AddHeader("Authorization", "Basic " + auth);
+            requestOrder.AddHeader("UserAgent", supplierid + " - SelfIntegration");
+            IRestResponse responseOrder = clientOrder.Execute(requestOrder) as RestResponse;
+            TrendyolOrder.Root results = JsonConvert.DeserializeObject<TrendyolOrder.Root>(responseOrder.Content);
+            prgBar.Maximum = results.totalElements;
+        run:
+            foreach (var trendOrder in results.content)
+            {
+                i++;
+                prgBar.Value = i;
+                //order package can be split,this is controlled
+                //sipariş paketi bölünebilir
+                clientOrder = new RestClient(url + supplierid + "/orders?status=UnPacked&orderNumber=" + trendOrder.orderNumber);
+                requestOrder = new RestRequest(Method.GET);
+                requestOrder.AddHeader("Accept", "application/json;charset=utf-8");
+                requestOrder.AddHeader("Authorization", "Basic " + auth);
+                requestOrder.AddHeader("UserAgent", supplierid + " - SelfIntegration");
+                responseOrder = clientOrder.Execute(requestOrder) as RestResponse;
+                TrendyolOrder.Root resultsOrder = JsonConvert.DeserializeObject<TrendyolOrder.Root>(responseOrder.Content);
+                //order package split
+                if (resultsOrder.totalElements > 0)
+                {
+                    //package split orders are used to learn shipping tracking numbers
+                    //paket bolmesi olan Orderler kargo takip numaralarını öğrenmek amacıyla order'a gönderilir 
+
+                    foreach (var trendUnpacked in resultsOrder.content)
+                    {
+                        clientOrder = new RestClient(url + supplierid + "/orders?orderNumber=" + trendUnpacked.orderNumber);
+                        requestOrder = new RestRequest(Method.GET);
+                        requestOrder.AddHeader("Accept", "application/json;charset=utf-8");
+                        requestOrder.AddHeader("Authorization", "Basic " + auth);
+                        requestOrder.AddHeader("UserAgent", supplierid + " - SelfIntegration");
+                        responseOrder = clientOrder.Execute(requestOrder) as RestResponse;
+                        TrendyolUnPackedOrder.Order resultsUnpacked = JsonConvert.DeserializeObject<TrendyolUnPackedOrder.Order>(responseOrder.Content);
+                        var cargoTrakingNumber = resultsUnpacked.content.Where(w => w.shipmentPackageStatus != "UnPacked" || w.shipmentPackageStatus != "Cancelled" || w.shipmentPackageStatus != "UnSupplied").Select(s => new { s.cargoTrackingNumber, s.cargoTrackingLink, s.cargoProviderName, s.cargoSenderNumber }).ToList();
+                        foreach (var item in cargoTrakingNumber)
+                        {
+                            //TODO: your's code/sizin kodlarınız
+                        }
+
+                    }
+                }
+
+                //Only the item of the order can be cancelled,this is controlled
+                //Verilen siparişin sadece kalemi cancelled olabilir kontrol edilir
+                clientOrder = new RestClient(url + supplierid + "/orders?status=Cancelled,UnSupplied&orderNumber=" + trendOrder.orderNumber);
+                requestOrder = new RestRequest(Method.GET);
+                requestOrder.AddHeader("Accept", "application/json;charset=utf-8");
+                requestOrder.AddHeader("Authorization", "Basic " + auth);
+                requestOrder.AddHeader("UserAgent", supplierid + " - SelfIntegration");
+                responseOrder = clientOrder.Execute(requestOrder) as RestResponse;
+                TrendyolOrder.Root resultsCancelled = JsonConvert.DeserializeObject<TrendyolOrder.Root>(responseOrder.Content);
+                foreach (var trendOdetay in trendOrder.lines)
+                {
+                    if (resultsCancelled.content.Count() > 0)
+                    {
+                        var cancelled = (resultsCancelled.content.FirstOrDefault().lines).Where(w => w.id == trendOdetay.id && w.sku == trendOdetay.sku).FirstOrDefault();
+                        //TODO: your's code/sizin kodlarınız
+
+                    }
+                }
+            }
+            if (page <= results.totalPages)
+            {
+                page++;
+                goto run;
+            }
+            #endregion
+
+            #region Delivered
+            page = 0;
+            i = 0;
+            prgBar.Value = 0;
+        // return request can be give in case of transport
+        //taşıma durumunda iade talebi verilebilir
+        run1:
+            clientOrder = new RestClient(url + supplierid + "/orders?status=Shipped&page=" + page);
+            requestOrder = new RestRequest(Method.GET);
+            requestOrder.AddHeader("Accept", "application/json;charset=utf-8");
+            requestOrder.AddHeader("Authorization", "Basic " + auth);
+            requestOrder.AddHeader("UserAgent", supplierid + " - SelfIntegration");
+            responseOrder = clientOrder.Execute(requestOrder) as RestResponse;
+            TrendyolOrder.Root resultsShippedOrder = JsonConvert.DeserializeObject<TrendyolOrder.Root>(responseOrder.Content);
+            prgBar.Maximum = results.totalElements;
+            if (resultsShippedOrder.totalElements > 0)
+            {
+                foreach (var sOrder in resultsShippedOrder.content)
+                {
+                    i++;
+                    prgBar.Value = i;
+
+                    #region  is there a refund/iadesi var mi
+                    clientOrder = new RestClient(url + supplierid + "/claims?claimItemStatus=Created&orderNumber=" + sOrder.orderNumber);
+                    requestOrder = new RestRequest(Method.GET);
+                    requestOrder.AddHeader("Accept", "application/json;charset=utf-8");
+                    requestOrder.AddHeader("Authorization", "Basic " + auth);
+                    requestOrder.AddHeader("UserAgent", supplierid + " - SelfIntegration");
+                    responseOrder = clientOrder.Execute(requestOrder) as RestResponse;
+                    TrendyolRebate.Order resultsRebateRequest = JsonConvert.DeserializeObject<TrendyolRebate.Order>(responseOrder.Content);
+                    #endregion
+                    #region  Is the refund confirmed/iadesi onaylandi mi
+                    clientOrder = new RestClient(url + supplierid + "/claims?claimItemStatus=Accepted&orderNumber=" + sOrder.orderNumber);
+                    requestOrder = new RestRequest(Method.GET);
+                    requestOrder.AddHeader("Accept", "application/json;charset=utf-8");
+                    requestOrder.AddHeader("Authorization", "Basic " + auth);
+                    requestOrder.AddHeader("UserAgent", supplierid + " - SelfIntegration");
+                    responseOrder = clientOrder.Execute(requestOrder) as RestResponse;
+                    TrendyolRebate.Order resultsRebate = JsonConvert.DeserializeObject<TrendyolRebate.Order>(responseOrder.Content);
+                    #endregion
+                    if (resultsRebateRequest.content.Count() > 0)
+                    {
+                        foreach (var RebateRequest in resultsRebateRequest.content)
+                        {
+                            foreach (var item in RebateRequest.items.Select(s => new { s.claimItems, s.orderLine }).ToList())
+                            {
+                                var rebateItems= String.Join(",", item.claimItems.Select(s => s.orderLineItemId));
+                            }
+                        }
+
+                        }
+                    else if (resultsRebate.content.Count() > 0)
+                    {
+                        foreach (var Rebate in resultsRebate.content)
+                        {
+                        }
+                    }
+
+                }
+            }
+            if (page <= results.totalPages)
+            {
+                page++;
+                goto run1;
+            }
+            #endregion
+
+            //It will be better to run it according to the web service status
+            //Web servis statülerine göre çalıştırmak daha iyi olacaktır
+
+            //Canceled,Not Procured states can be called together
+            //Returned must be called separately
+            //Cancelled,UnSupplied statuleri birlikte çağrılabilir
+            //Returned ayrı çağrılmalıdır
         }
     }
 }
---
+++ Trendyol_Api/Trendyol_Api/Models.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trendyol_Api
+{
+   public class Models
+    {
+        public class TrendyolOrder
+        {
+          
+            public class ShipmentAddress
+            {
+                public int id { get; set; }
+                public string firstName { get; set; }
+                public string lastName { get; set; }
+                public string address1 { get; set; }
+                public string address2 { get; set; }
+                public string city { get; set; }
+                public int cityCode { get; set; }
+                public string district { get; set; }
+                public int districtId { get; set; }
+                public string postalCode { get; set; }
+                public string countryCode { get; set; }
+                public string fullName { get; set; }
+                public string fullAddress { get; set; }
+            }
+
+            public class InvoiceAddress
+            {
+                public int id { get; set; }
+                public string firstName { get; set; }
+                public string lastName { get; set; }
+                public string company { get; set; }
+                public string address1 { get; set; }
+                public string address2 { get; set; }
+                public string city { get; set; }
+                public string district { get; set; }
+                public string postalCode { get; set; }
+                public string countryCode { get; set; }
+                public string fullName { get; set; }
+                public string fullAddress { get; set; }
+            }
+
+            public class DiscountDetail
+            {
+                public double lineItemPrice { get; set; }
+                public double lineItemDiscount { get; set; }
+            }
+
+            public class Line
+            {
+                public int quantity { get; set; }
+                public int salesCampaignId { get; set; }
+                public string productSize { get; set; }
+                public string merchantSku { get; set; }
+                public string productName { get; set; }
+                public int productCode { get; set; }
+                public int merchantId { get; set; }
+                public double amount { get; set; }
+                public double discount { get; set; }
+                public double price { get; set; }
+                public List<DiscountDetail> discountDetails { get; set; }
+                public string currencyCode { get; set; }
+                public string productColor { get; set; }
+                public int id { get; set; }
+                public string sku { get; set; }
+                public int vatBaseAmount { get; set; }
+                public string barcode { get; set; }
+                public string orderLineItemStatusName { get; set; }
+            }
+
+            public class PackageHistory
+            {
+                public object createdDate { get; set; }
+                public string status { get; set; }
+            }
+
+            public class Content
+            {
+                public ShipmentAddress shipmentAddress { get; set; }
+                public string orderNumber { get; set; }
+                public double grossAmount { get; set; }
+                public double totalDiscount { get; set; }
+                public double totalPrice { get; set; }
+                public object taxNumber { get; set; }
+                public InvoiceAddress invoiceAddress { get; set; }
+                public string customerFirstName { get; set; }
+                public string customerEmail { get; set; }
+                public int customerId { get; set; }
+                public string customerLastName { get; set; }
+                public int id { get; set; }
+                public long cargoTrackingNumber { get; set; }
+                public string cargoTrackingLink { get; set; }
+                public string cargoSenderNumber { get; set; }
+                public string cargoProviderName { get; set; }
+                public List<Line> lines { get; set; }
+                public long orderDate { get; set; }
+                public string tcIdentityNumber { get; set; }
+                public string currencyCode { get; set; }
+                public List<PackageHistory> packageHistories { get; set; }
+                public string shipmentPackageStatus { get; set; }
+                public long estimatedDeliveryStartDate { get; set; }
+                public long estimatedDeliveryEndDate { get; set; }
+                public string deliveryAddressType { get; set; }
+                public long agreedDeliveryDate { get; set; }
+            }
+
+            public class Root
+            {
+                public int page { get; set; }
+                public int size { get; set; }
+                public int totalPages { get; set; }
+                public int totalElements { get; set; }
+                public List<Content> content { get; set; }
+            }
+
+
+        }
+
+        public class TrendyolUnPackedOrder
+        {
+            public class ShipmentAddress
+            {
+                public Nullable<int> id { get; set; }
+                public string firstName { get; set; }
+                public string lastName { get; set; }
+                public string address1 { get; set; }
+                public string address2 { get; set; }
+                public string city { get; set; }
+                public Nullable<int> cityCode { get; set; }
+                public string district { get; set; }
+                public Nullable<int> districtId { get; set; }
+                public string postalCode { get; set; }
+                public string countryCode { get; set; }
+                public Nullable<int> neighborhoodId { get; set; }
+                public string neighborhood { get; set; }
+                public string fullAddress { get; set; }
+                public string fullName { get; set; }
+            }
+
+            public class InvoiceAddress
+            {
+                public Nullable<int> id { get; set; }
+                public string firstName { get; set; }
+                public string lastName { get; set; }
+                public string company { get; set; }
+                public string address1 { get; set; }
+                public string address2 { get; set; }
+                public string city { get; set; }
+                public Nullable<int> cityCode { get; set; }
+                public string district { get; set; }
+                public Nullable<int> districtId { get; set; }
+                public string postalCode { get; set; }
+                public string countryCode { get; set; }
+                public Nullable<int> neighborhoodId { get; set; }
+                public string neighborhood { get; set; }
+                public string phone { get; set; }
+                public string fullAddress { get; set; }
+                public string fullName { get; set; }
+            }
+
+            public class DiscountDetail
+            {
+                public Nullable<double> lineItemPrice { get; set; }
+                public Nullable<int> lineItemDiscount { get; set; }
+            }
+
+            public class Line
+            {
+                public List<string> packageItemIds { get; set; }
+                public List<int> lineItemIds { get; set; }
+                public Nullable<int> quantity { get; set; }
+                public Nullable<int> salesCampaignId { get; set; }
+                public string productSize { get; set; }
+                public string merchantSku { get; set; }
+                public string productName { get; set; }
+                public Nullable<int> productCode { get; set; }
+                public Nullable<int> merchantId { get; set; }
+                public Nullable<double> amount { get; set; }
+                public Nullable<int> discount { get; set; }
+                public List<DiscountDetail> discountDetails { get; set; }
+                public string currencyCode { get; set; }
+                public Nullable<int> id { get; set; }
+                public string sku { get; set; }
+                public Nullable<double> vatBaseAmount { get; set; }
+                public string barcode { get; set; }
+                public string orderLineItemStatusName { get; set; }
+                public Nullable<double> price { get; set; }
+            }
+
+            public class PackageHistory
+            {
+                public object createdDate { get; set; }
+                public string status { get; set; }
+            }
+
+            public class Content
+            {
+                public ShipmentAddress shipmentAddress { get; set; }
+                public string orderNumber { get; set; }
+                public Nullable<double> grossAmount { get; set; }
+                public Nullable<double> totalDiscount { get; set; }
+                public object taxNumber { get; set; }
+                public InvoiceAddress invoiceAddress { get; set; }
+                public string customerFirstName { get; set; }
+                public string customerEmail { get; set; }
+                public Nullable<int> customerId { get; set; }
+                public string customerLastName { get; set; }
+                public string customerNote { get; set; }
+                public Nullable<int> id { get; set; }
+                public Nullable<int> orderId { get; set; }
+                public object cargoTrackingNumber { get; set; }
+                public string cargoProviderName { get; set; }
+                public List<Line> lines { get; set; }
+                public object orderDate { get; set; }
+                public string tcIdentityNumber { get; set; }
+                public string currencyCode { get; set; }
+                public List<PackageHistory> packageHistories { get; set; }
+                public string shipmentPackageStatus { get; set; }
+                public string deliveryType { get; set; }
+                public Nullable<int> timeSlotId { get; set; }
+                public string scheduledDeliveryStoreId { get; set; }
+                public object estimatedDeliveryStartDate { get; set; }
+                public object estimatedDeliveryEndDate { get; set; }
+                public string deliveryAddressType { get; set; }
+                public Nullable<double> totalPrice { get; set; }
+                public string cargoTrackingLink { get; set; }
+                public string cargoSenderNumber { get; set; }
+            }
+
+            public class Order
+            {
+                public Nullable<int> page { get; set; }
+                public Nullable<int> size { get; set; }
+                public Nullable<int> totalPages { get; set; }
+                public Nullable<int> totalElements { get; set; }
+                public List<Content> content { get; set; }
+            }
+        }
+
+        public class TrendyolRebate
+        {
+            // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+            public class LineItemStatus
+            {
+                public string name { get; set; }
+            }
+
+            public class LineItem
+            {
+                public Nullable<int> id { get; set; }
+                public LineItemStatus lineItemStatus { get; set; }
+            }
+
+            public class OrderLine
+            {
+                public Nullable<int> id { get; set; }
+                public string productName { get; set; }
+                public string barcode { get; set; }
+                public string merchantSku { get; set; }
+                public string productSize { get; set; }
+                public Nullable<double> price { get; set; }
+                public Nullable<double> vatBaseAmount { get; set; }
+                public Nullable<int> salesCampaignId { get; set; }
+                public string productCategory { get; set; }
+                public List<LineItem> lineItems { get; set; }
+            }
+
+            public class CustomerClaimItemReason
+            {
+                public Nullable<int> id { get; set; }
+                public string name { get; set; }
+                public Nullable<int> externalReasonId { get; set; }
+                public string code { get; set; }
+            }
+
+            public class TrendyolClaimItemReason
+            {
+                public Nullable<int> id { get; set; }
+                public string name { get; set; }
+                public Nullable<int> externalReasonId { get; set; }
+                public string code { get; set; }
+            }
+
+            public class ClaimItemStatus
+            {
+                public string name { get; set; }
+            }
+
+            public class ClaimItem
+            {
+                public string id { get; set; }
+                public Nullable<int> orderLineItemId { get; set; }
+                public CustomerClaimItemReason customerClaimItemReason { get; set; }
+                public TrendyolClaimItemReason trendyolClaimItemReason { get; set; }
+                public ClaimItemStatus claimItemStatus { get; set; }
+                public string note { get; set; }
+                public bool resolved { get; set; }
+            }
+
+            public class Item
+            {
+                public OrderLine orderLine { get; set; }
+                public List<ClaimItem> claimItems { get; set; }
+            }
+
+            public class Content
+            {
+                public string id { get; set; }
+                public string orderNumber { get; set; }
+                public object orderDate { get; set; }
+                public string customerFirstName { get; set; }
+                public string customerLastName { get; set; }
+                public object claimDate { get; set; }
+                public object cargoTrackingNumber { get; set; }
+                public string cargoProviderName { get; set; }
+                public Nullable<int> orderShipmentPackageId { get; set; }
+                public List<Item> items { get; set; }
+            }
+
+            public class Order
+            {
+                public Nullable<int> totalElements { get; set; }
+                public Nullable<int> totalPages { get; set; }
+                public Nullable<int> page { get; set; }
+                public Nullable<int> size { get; set; }
+                public List<Content> content { get; set; }
+            }
+
+
+        }
+    }
+}
